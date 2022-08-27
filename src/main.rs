5c93a975ace22646c625a23450be66ab7d440705
Deep Gaurav <deepgauravraj@gmail.com>
2022-08-27 18:36:32 +0000

    add downloader

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,17 @@ use async_graphql::{
 };
 use async_graphql_axum::{GraphQLRequest, GraphQLResponse, GraphQLSubscription};
 use axum::{
+    body::{boxed, Body, BoxBody},
+    extract::Path,
+    http::{Request, Response, StatusCode},
     response::{self, IntoResponse},
     routing::get,
     Extension, Router, Server,
 };
 use dashmap::DashMap;
 use structures::{MainSchema, SubscriptionRoot};
+use tower::ServiceExt;
+use transmission::Torrent;
 
 use crate::{
     context::SharedData,
@@ -35,15 +40,16 @@ async fn graphql_playground() -> impl IntoResponse {
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
-
+    let torrents = Arc::new(DashMap::new());
+    let download_dir = std::env::var("TOREXPO_DOWNLOAD_DIR").unwrap_or_else(|_| "downloads".into());
     let transmission_config = transmission::ClientConfig::new()
         .app_name("torexpo")
-        .download_dir(&std::env::var("TOREXPO_DOWNLOAD_DIR").unwrap_or_else(|_| "downloads".into()))
+        .download_dir(&download_dir)
         .config_dir(&std::env::var("TOREXPO_CONFIG_DIR").unwrap_or_else(|_| "config".into()));
     let transmission_client = transmission::Client::new(transmission_config);
     let data = SharedData {
         client: transmission_client,
-        torrents: Arc::new(DashMap::new()),
+        torrents: torrents.clone(),
     };
 
     let schema = Schema::build(QueryRoot, MutationRoot, SubscriptionRoot)
@@ -53,6 +59,10 @@ async fn main() {
     let app = Router::new()
         .route("/", get(graphql_playground).post(graphql_handler))
         .route("/ws", GraphQLSubscription::new(schema.clone()))
+        .route(
+            "/download/:torrent_id/:file_id",
+            get(move |path, req| serve_file(torrents, download_dir, path, req)),
+        )
         .layer(Extension(schema));
 
     let port = std::env::var("TOREXPO_PORT").unwrap_or_else(|_| "8080".into());
@@ -60,4 +70,32 @@ async fn main() {
         .serve(app.into_make_service())
         .await
         .unwrap();
+}
+
+async fn serve_file(
+    torrents: Arc<DashMap<i32, Torrent>>,
+    download_dir: String,
+    Path((torrent_id, file_id)): Path<(i32, usize)>,
+    req: Request<Body>,
+) -> Result<Response<BoxBody>, (StatusCode, String)> {
+    match torrents
+        .get(&torrent_id)
+        .map(|torrent| torrent.info().files)
+    {
+        Some(files) => match files.get(file_id) {
+            Some(file) => {
+                let path = std::path::Path::new(&download_dir).join(&file.name);
+                let servefile = tower_http::services::ServeFile::new(path);
+                match servefile.oneshot(req).await {
+                    Ok(res) => Ok(res.map(boxed)),
+                    Err(err) => Err((
+                        StatusCode::INTERNAL_SERVER_ERROR,
+                        format!("Something went wrong: {}", err),
+                    )),
+                }
+            }
+            None => Err((StatusCode::NOT_FOUND, "File to download Not found".into())),
+        },
+        None => Err((StatusCode::NOT_FOUND, "Torrent not found".to_string())),
+    }
 }
